4d2c4d8e3b3599a001c5b998f0cc8f8826886427
Maksym <maximz216@gmail.com>
2025-01-14 16:35:59 +0200

    First comit

--- GitJavaTopic3.java
+++ GitJavaTopic3.java
@@ -2,6 +2,8 @@ package GitJavaTopic3;
 
 public class GitJavaTopic3 {
     public static void main(String[] args) {
+
         System.out.println("Hello world");
+        System.out.println("Hello Max");
     }
 }
